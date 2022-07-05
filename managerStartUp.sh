8aacd1154ef65ec81daaa3af9f655640753223a3
Karl Olson <76186248+KarlOlson@users.noreply.github.com>
2022-07-04 21:18:25 -0400

    Update managerStartUp.sh

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -10,20 +10,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get update
-sudo apt-get -y install docker-compose
-sudo wget https://github.com/docker/compose/releases/download/v2.5.1/docker-compose-linux-x86_64 -P ~/
-sudo mv ~/docker-compose-linux-x86_64 ~/docker-compose
-sudo chmod +x ~/docker-compose
-sudo mv ~/docker-compose /usr/local/bin/docker-compose
-sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-#sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-#sudo apt -y install docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker-compose
 #curl -L "https://github.com/docker/compose/releases/download/$(curl https://github.com/docker/compose/releases | grep -m1 '<a href="/docker/compose/releases/download/' | grep -o 'v[0-9:].[0-9].[0-9]')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#sudo ln -fs /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 sudo docker swarm init --advertise-addr=$MANAGER_IP
 
@@ -36,6 +28,3 @@ do
 done
 # Create docker registry
 sudo docker service create --name registry --publish published=5000,target=5000 registry:2
-
-sudo chmod +x deploy.sh
-sudo ./deploy.sh
